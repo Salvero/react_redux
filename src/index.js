adcd634045d46df0394657533fdb07ef531ed908
Salman Akram <salmanit2012@gmail.com>
2016-01-08 01:12:39 -0500

    components structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyCIg2PTXq-ekmKG4fI9mzehVWsi6gVLu7k';
+
+const App = () => {
+  return <div>Hi Matt!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
